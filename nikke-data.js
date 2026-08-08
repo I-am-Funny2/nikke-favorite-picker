0dd794f363cfc542fe321b8696db9921368976e0
I-am-Funny2 <seggsybwoi@gmail.com>
2026-08-09 01:28:24 +0300

    Create nikke-data.js

---
+++ nikke-data.js
@@ -0,0 +1,5 @@
+var items = [
+    {id: 'rapi', name: 'Rapi', image: 'images/nikke/rapi.png'},
+    {id: 'anis', name: 'Anis', image: 'images/nikke/anis.png'},
+    {id: 'neon', name: 'Neon', image: 'images/nikke/neon.png'}
+];
