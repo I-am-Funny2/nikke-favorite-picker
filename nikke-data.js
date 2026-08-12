77fb4131f93fa342a28262d14bb00a4f7e06d71d
I-am-Funny2 <seggsybwoi@gmail.com>
2026-08-12 22:47:06 +0300

    Add first NIKKE roster entries

--- nikke-data.js
+++ nikke-data.js
@@ -1,5 +1,11 @@
 var items = [
     {id: 'rapi', name: 'Rapi', image: 'images/nikke/fullbody/rapi.png'},
     {id: 'anis', name: 'Anis', image: 'images/nikke/fullbody/anis.png'},
-    {id: 'neon', name: 'Neon', image: 'images/nikke/fullbody/neon.png'}
+    {id: 'neon', name: 'Neon', image: 'images/nikke/fullbody/neon.png'},
+
+    {id: 'delta', name: 'Delta', image: 'images/nikke/fullbody/delta.png'},
+    {id: 'signal', name: 'Signal', image: 'images/nikke/fullbody/signal.png'},
+    {id: 'poli', name: 'Poli', image: 'images/nikke/fullbody/poli.png'},
+    {id: 'miranda', name: 'Miranda', image: 'images/nikke/fullbody/miranda.png'},
+    {id: 'd', name: 'D', image: 'images/nikke/fullbody/d.png'}
 ];
