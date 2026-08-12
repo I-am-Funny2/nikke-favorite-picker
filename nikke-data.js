e3cd3cf03653f34410d3cad489dfaae040c668c6
I-am-Funny2 <seggsybwoi@gmail.com>
2026-08-12 20:53:20 +0300

    Update image paths for Nikke characters

--- nikke-data.js
+++ nikke-data.js
@@ -1,5 +1,5 @@
 var items = [
-    {id: 'rapi', name: 'Rapi', image: 'images/nikke/rapi.png'},
-    {id: 'anis', name: 'Anis', image: 'images/nikke/anis.png'},
-    {id: 'neon', name: 'Neon', image: 'images/nikke/neon.png'}
+    {id: 'rapi', name: 'Rapi', image: 'images/nikke/fullbody/rapi.png'},
+    {id: 'anis', name: 'Anis', image: 'images/nikke/fullbody/anis.png'},
+    {id: 'neon', name: 'Neon', image: 'images/nikke/fullbody/neon.png'}
 ];
