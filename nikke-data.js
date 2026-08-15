0b3076195fbd505318ed2da44699093bfb07d80a
I-am-Funny2 <seggsybwoi@gmail.com>
2026-08-15 12:00:25 +0300

    Batch 4 added

--- nikke-data.js
+++ nikke-data.js
@@ -19,5 +19,21 @@ var items = [
     {id: 'liter', name: 'Liter', image: 'images/nikke/fullbody/liter.png'},
     {id: 'emma', name: 'Emma', image: 'images/nikke/fullbody/emma.png'},
     {id: 'vesti', name: 'Vesti', image: 'images/nikke/fullbody/vesti.png'},
-    {id: 'eunhwa', name: 'Eunhwa', image: 'images/nikke/fullbody/eunhwa.png'}
+    {id: 'eunhwa', name: 'Eunhwa', image: 'images/nikke/fullbody/eunhwa.png'},
+
+    {id: 'laplace', name: 'Laplace', image: 'images/nikke/fullbody/laplace.png'},
+    {id: 'drake', name: 'Drake', image: 'images/nikke/fullbody/drake.png'},
+    {id: 'maxwell', name: 'Maxwell', image: 'images/nikke/fullbody/maxwell.png'},
+    {id: 'crow', name: 'Crow', image: 'images/nikke/fullbody/crow.png'},
+    {id: 'jackal', name: 'Jackal', image: 'images/nikke/fullbody/jackal.png'},
+    {id: 'viper', name: 'Viper', image: 'images/nikke/fullbody/viper.png'},
+    {id: 'n102', name: 'N102', image: 'images/nikke/fullbody/n102.png'},
+    {id: 'anne-miracle-fairy', name: 'Anne: Miracle Fairy', image: 'images/nikke/fullbody/anne-miracle-fairy.png'},
+    {id: 'mary', name: 'Mary', image: 'images/nikke/fullbody/mary.png'},
+    {id: 'pepper', name: 'Pepper', image: 'images/nikke/fullbody/pepper.png'},
+    {id: 'mary-bay-goddess', name: 'Mary: Bay Goddess', image: 'images/nikke/fullbody/mary-bay-goddess.png'},
+    {id: 'sugar', name: 'Sugar', image: 'images/nikke/fullbody/sugar.png'},
+    {id: 'milk', name: 'Milk', image: 'images/nikke/fullbody/milk.png'},
+    {id: 'frima', name: 'Frima', image: 'images/nikke/fullbody/frima.png'},
+    {id: 'julia', name: 'Julia', image: 'images/nikke/fullbody/julia.png'}
 ];
